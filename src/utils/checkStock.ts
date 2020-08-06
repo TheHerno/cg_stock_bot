4e4139d71cc89dd5b7cad7733c33a621684b410f
Hernan <hernanlistorti@gmail.com>
2020-08-06 19:44:56 -0300

    fixed checkStock

--- src/utils/checkStock.ts
+++ src/utils/checkStock.ts
@@ -1,7 +1,7 @@
 const checkStock = async (product: string): Promise<Boolean> => {
   const res = await fetch(`https://compragamer.com/producto/${product}`)
   const html = await res.text()
-  let stock = html.includes("Sumar al carrito") ? true : false
+  let stock = html.includes("Sumar al carrito")
   return stock
 }
 
